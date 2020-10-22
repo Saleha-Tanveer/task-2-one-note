52ed353158e055c5016971dc9e6f2e7cc8349e9c
Muhammad Bilal <r.masadbilal@gmail.com>
2020-10-22 14:56:12 +0500

    tables name changed

--- Models/NoteModel.py
+++ Models/NoteModel.py
@@ -3,11 +3,10 @@ from database import db
 # from run import ma
 
 class Note(db.Model):
-    __tablename__='OneNote'
+    __tablename__='Note hi note zindagi mein ... aahahhaha'
     id =db.Column('note_id',db.Integer,primary_key=True)
     category=db.Column('category',db.String(50),nullable=False)
     note_title=db.Column('note_title',db.String(50),nullable=False)
-
     description=db.Column('description',db.Text(),nullable=False)s
     date_created=db.Column('date_created',db.DateTime,default=datetime.now)
     user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
